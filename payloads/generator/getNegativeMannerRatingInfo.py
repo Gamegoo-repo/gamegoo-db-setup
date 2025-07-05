1ac9da27ce906a3cf9dba4c874c0be9af100b99e
Eunjin3395 <jennyeunjin@khu.ac.kr>
2025-07-05 13:05:40 +0900

    매너평가 관련 payload 생성 코드 추가

--- payloads/generator/getNegativeMannerRatingInfo.py
+++ payloads/generator/getNegativeMannerRatingInfo.py
@@ -9,13 +9,18 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'
 from modules import db_fetcher
 from modules import random_modules as rm
 
-VUSERS = 170
+VUSERS = 70
 PAYLOADS_PER_USER = 20
 PAYLOAD_NAME = os.path.splitext(os.path.basename(__file__))[0] # 현재 파일 이름에서 확장자 제거
 
-# step 1: member id 조회
+MEMBER_PASSWORD="12345678"
+
+# step 1: member id,email 조회
 print(f"[1] Fetching member ids...")
-member_ids = [row[0] for row in db_fetcher.fetch_columns("member", ["member_id"])]
+members = db_fetcher.fetch_columns("member", ["member_id","email"]) # [member_id,email]
+
+member_ids = list(row[0] for row in members)
+member_emails = list(row[1] for row in members)
 
 # payload json 생성
 file_name = f"{PAYLOAD_NAME}_{VUSERS}vus.json"
@@ -29,14 +34,19 @@ payload_data = []
 
 for _ in range(VUSERS):
     payloads = []
-    for _ in range(PAYLOADS_PER_USER):
+    i = rm.sample_integer(1,len(members)-1)
+    while len(payloads) < PAYLOADS_PER_USER:
         random_member_id = random.choice(member_ids)
-        payloads.append({
-            "path": {
-                "memberId": random_member_id
-            }
+        if random_member_id != member_ids[i]:
+            payloads.append({
+                "path": {
+                    "memberId": random_member_id
+                }
         })
+            
     payload_data.append({
+        "email": member_emails[i],
+        "password": MEMBER_PASSWORD,
         "payloads": payloads
     })
 
